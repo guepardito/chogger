8c896b237f42e25d887a83ce8825e31eed95c696
guepardo <rafagi04@outlook.es>
2025-04-20 16:32:52 +0200

    Fixed build scripts

--- build.ps1
+++ build.ps1
@@ -1,2 +1,9 @@
+if (-Not (Test-Path -Path "./build")) {
+    New-Item -ItemType Directory -Force -Path "./build"
+}
+
+# Compile the source code
 gcc -Wall -Wextra -c chogger.c -o ./build/chogger.o
+
+# Create the static library
 ar rcs ./build/libchogger.a ./build/chogger.o
--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+mkdir -p ./build/
+
 gcc -Wall -Wextra -c chogger.c -o ./build/chogger.o
 ar rcs ./build/libchogger.a ./build/chogger.o
